b4a6be0f0482653b3602606076b849fee9e393ce
Gorka Plans <gorkaplans@MacBook-MacBook-Pro-de-Gorka.local>
2020-11-25 23:10:04 +0100

    fixing bugs

--- forty_web/src/components/Home/Home.js
+++ forty_web/src/components/Home/Home.js
@@ -7,17 +7,23 @@ const Home = () => {
         
         <div className="columns">
         <div className="column1">
-          <span className="block1">
-          </span>
+          <span className="block1"></span>
           <span className="block2" />
         </div>
         <div className="column2">
           <h1>COMING SOON</h1>
-
         </div>
-        <div className="column3">
-          <h2>Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat turpis proin tortor torquent neque semper, vestibulum cum phasellus et malesuada rutrum augue habitasse posuere justo convallis natoque. Netus semper metus urna quisque dui luctus orci interdum, fames aliquam massa ante ac turpis himenaeos, molestie nisi ligula suscipit vivamus tincidunt risus.
-          </h2>
+        <div className="column1">
+          <span className="block3">
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat turpis proin tortor torquent neque semper, vestibulum cum phasellus et malesuada rutrum augue habitasse posuere justo convallis natoque. Netus semper metus urna quisque dui luctus orci interdum, fames aliquam massa ante ac turpis himenaeos, molestie nisi ligula suscipit vivamus tincidunt risus.Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat turpis proin tortor torquent neque semper, vestibulum cum phasellus et malesuada rutrum augue habitasse posuere justo convallis natoque.</p>
+            </span>
+
+            <span className="block4">
+                <p>hello@whitemoose.tv</p>
+                <p>653 05 71 04 / 93 148 36 43</p>
+                <p>C/ Pereda 4, Sabadell 08203, Barcelona.</p>
+          </span>
+          
         </div>
       </div>
     
