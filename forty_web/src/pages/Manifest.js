20dc3d3c3552d04c7ae2b71cba7c82640e2c075b
Gorka Plans <gorkaplans@MacBook-MacBook-Pro-de-Gorka.local>
2020-12-23 22:32:52 +0100

    khjgjkh

--- forty_web/src/pages/Manifest.js
+++ forty_web/src/pages/Manifest.js
@@ -1,12 +1,21 @@
 import './Manifest.scss'
 import Menu from '../components/Menu';
+import MainLayout from '../components/MainLayout'
 
 const Manifest = () => {
 
   const menu = ['MANIFEST', 'PROJECTS', 'CONTACT']
 
 return (
-  <Menu titles={menu}></Menu>
+  <>
+    <MainLayout>
+      <div className='manifest-container'>
+      <Menu titles={menu}></Menu>
+      <div className='picture-manifest'></div>
+      <div className='text-manifest'></div>
+      </div>
+    </MainLayout>
+  </>
   );
 }
  
